a60ef088fabcb3ac44d97d81927eb873702f7bfd
faithtremmel <78774537+faithtremmel@users.noreply.github.com>
2022-03-09 19:24:38 -0500

    Added map

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
-d3.csv()
+
+
+
+d3.csv('data/occurrences.csv')
 .then(data => {
     data.forEach(d => {
+      d.id = +d.id
 
-    })
-})
-.catch(error => console.error(error));
+      if(d.decimalLatitude == "null"){
+        d.decimalLatitude = 99999999
+      }
+      if(d.decimalLongitude == "null"){
+        d.decimalLongitude = 99999999
+      }
+
+      if(d.year == "null" || d.year == ""){
+        console.log(d.eventDate)
+        let date = new Date(d.eventDate) 
+        console.log(date.getFullYear())
+        if(!date.getFullYear()){
+          // TODO parse string for year and month
+          // d.year = d.eventDate
+          // d.month = d.eventDate
+        }
+      }
+
+      d.year = +d.year
+
+      d.latitude = +d.decimalLatitude;
+      d.longitude = +d.decimalLongitude; 
+
+    });
+
+    leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+
+
+  })
+  .catch(error => console.error(error));
