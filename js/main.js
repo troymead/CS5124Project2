ab8dd6c7a5f67c37cda6110a59fb01931f8ffd60
troymead <tspringmeadows@gmail.com>
2022-03-21 14:00:16 -0400

    updated occurrences.csv

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 d3.csv()
 .then(data => {
     data.forEach(d => {
-
+        d.id = +d.id;
+        d.taxonID = +d.taxonID;
+        d.year = +d.year;
+        if (d.month == '') {
+            d.month = null;
+        } else {
+            d.month = +d.month;
+        }
+        if (d.day == '') {
+            d.month = null;
+        } else {
+            d.day = +d.day;
+        }
     })
 })
 .catch(error => console.error(error));
